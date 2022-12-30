a72cf6a1e720f8b7f9f325e289267c216c7848ce
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-12-31 00:43:23 +0500

    authnticated routes --- redux setup -- auth layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoutes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/dashboard" element={<ProtectedRoute component={Dashboard} />} />
+      <Route exact path="/models" element={<ProtectedRoute component={Models} />} />
+      <Route exact path="/logs" element={<ProtectedRoute component={Logs} />} />
+    </Routes>
   );
 }
+
+const Home = () => <h1>HOME</h1>
+const Dashboard = () => <h1>DASHBOARD</h1>
+const Models = () => <h1>Models</h1>
+const Logs = () => <h1>Logs</h1>
 
 export default App;
